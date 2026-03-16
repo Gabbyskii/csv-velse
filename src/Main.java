4e00f1e0ab1761f906e754fd8157e92e33d1500e
Gabbyskii <gkadiga1@gmail.com>
2026-03-16 11:55:08 +0100

    methods added to change csv

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,79 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    // erklæret Scanner udenfor main metoden og gjort static så den er accessible fra metoder
+    static Scanner reader; // = new Scanner(System.in);
+    static String header;
+
     static void main() {
         ArrayList<Player> team = new ArrayList<>();
 
         File file = new File("data/playerdata.csv");
         try {
-            Scanner reader = new Scanner(file);
+            // fjernet scanner herfra: Scanner reader = new Scanner(file);
 
-            reader.nextLine(); // skipper header i filen
-            while (reader.hasNextLine()){
+            reader = new Scanner(file);
+            header = reader.nextLine(); // skipper header i filen
+
+            while (reader.hasNextLine()) {
                 String line = reader.nextLine(); // vi vil kun have: "Phillip", "hb", 168,
                 String[] data = line.split(",");
 
                 String name = data[0];
-                String position = data[1];
+                String position = data[1].trim();
                 int heartrate = Integer.parseInt(data[2].trim());
+                int collision = Integer.parseInt(data[3]);
+                int km = Integer.parseInt(data[4]);
+                int speed = Integer.parseInt(data[5]);
+                int yCard = Integer.parseInt(data[6]);
+                int rCard = Integer.parseInt(data[7]);
 
-                Player p = new Player(name, position, heartrate);
+                Player p = new Player(name, position, heartrate, collision,km,speed,yCard,rCard);
                 team.add(p);
 
-                System.out.println("===Team one===");
+                System.out.println("\n===Team one===");
+                /*team.get(0).setName("Mbappe");
+                team.get(0).setPosition("Goalkeeper");
+                team.get(0).setHeartrate(165);
+                team.get(1).setPosition("hb");
+                team.get(2).setPosition("Angriber");*/
                 displayTeam(team);
+                saveData(team);
 
             }
-
+            reader.close();
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
+
     }
 
-
-    public static void displayTeam(ArrayList<Player>teams){
-        for (Player pl: teams){
+    public static void displayTeam(ArrayList<Player> teams) {
+        for (Player pl: teams) {
             System.out.println(pl);
         }
     }
 
+    public static void saveData(ArrayList<Player> teams) {
+        try {
+            FileWriter fileWriter = new FileWriter("data/playerdata.csv");
+
+            for (Player t: teams) {
+                fileWriter.write(t.getName() + ", " + t.getPosition()
+                        + " , "+ t.getHeartrate()+ t.getKm() + t.getAvrTopSpeed()
+                        + t.getyCard() + t.getrCard()+ t.getCollisionCount()+" \n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 
 }
+
--- src/Player.java
+++ src/Player.java
@@ -3,11 +3,22 @@ public class Player {
    private String name;
    private String position;
    private int heartrate;
+   private int collisionCount;
+   private int km;
+   private int avrTopSpeed;
+   private int yCard;
+   private int rCard;
 
-    public Player(String name, String position, int heartrate) {
+
+    public Player(String name, String position, int heartrate, int collisionCount, int km, int avrTopSpeed, int yCard, int rCard) {
         this.name = name;
         this.position = position;
         this.heartrate = heartrate;
+        this.collisionCount = collisionCount;
+        this.km = km;
+        this.avrTopSpeed = avrTopSpeed;
+        this.yCard = yCard;
+        this.rCard = rCard;
     }
 
     public String getName() {
@@ -34,10 +45,38 @@ public class Player {
         this.heartrate = heartrate;
     }
 
+    public int getrCard() {
+        return rCard;
+    }
+
+    public int getyCard() {
+        return yCard;
+    }
+
+    public int getAvrTopSpeed() {
+        return avrTopSpeed;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public int getCollisionCount() {
+        return collisionCount;
+    }
 
     @Override
     public String toString() {
-        return "Player: " + name + position + " "+ heartrate;
+        //return "Player: " + name +","+ position + ", "+ heartrate;
+        String s = "Player: "+ this.name;
+        s += " (Position: "+ position + " &";
+        s += " HeartRate: "+ heartrate;
+        s += " CCount: "+ collisionCount;
+        s+= " Km: "+ km;
+        s+= " Top speed: "+avrTopSpeed;
+        s+= " yellow card: "+ yCard;
+        s+= " red card: "+ rCard+")\n";
+        return s;
     }
 
 }
